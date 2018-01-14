b855c51c896fce330d4ed7875622f583afeb4e86
Chris Wolf <chriswolf@Chriss-MacBook-Pro.local>
2018-01-14 07:25:50 -0500

    "Completed: Project 4 - Weather Forecast"

---
+++ master/FrontEndDevelopmentCertification/Project4_WeatherForecast/README.txt
@@ -0,0 +1,10 @@
+Basic Weather App
+
+A webpage that will do the following:
+    - Display current geolocation to user
+    - Display current temperature in Celsius to user
+    - Display current precipitation to user in form of image
+    - Allow user to translate between Celsius and Fahrenheit
+
+Chris Wolf
+chriswolfdesign@gmail.com
--- master/FrontEndDevelopmentCertification/Project4_WeatherForecast/index.js
+++ master/FrontEndDevelopmentCertification/Project4_WeatherForecast/index.js
@@ -1,22 +1,52 @@
+// Globals
+var latitude;
+var longitude;
+var ajaxURL = "";
+var celsiusTemperature;
+var fahrenheitTemperature;
+var isCelsius = true;
 
-// When page is finished loading
-$(document).ready(function () {
+$(document).ready(function() {
+
+	// retrieve geographical coordinates
 	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function (position) {
-			var latitude = position.coords.latitude;
-			var longitude = position.coords.longitude;
+		navigator.geolocation.getCurrentPosition(function(position) {
+			latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
 
-			// TODO: Change AJAX request to weather request 
+			ajaxURL = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude +
+				 "&lon=" + longitude;
+
+			// Retrieve weather information
 			$.ajax({
-				url: "https://talaikis.com/api/quotes/random/",
+				type: "GET",
+				url: ajaxURL,
 				async: true,
 				cache: false,
 				success: function(data) {
-					console.log("AJAX request has been successful!");
-					var quote = data.quote;
-					$("body").html(quote);
+					$("#location").html("Current weather in " + data.name +
+									    " is: ");
+					$("#convert").html("Convert to Fahrenheit");
+					celsiusTemperature = data.main.temp;
+					fahrenheitTemperature = Math.floor(data.main.temp * (9/5) + 32);
+					$("#temperature").html(celsiusTemperature + " &#8451");
+					$("#weather-icon").html("<img src=" + data.weather[0].icon + "/>");
+
 				}
 			});
 		});
 	}
+
+	// Button to convert between
+	$("#convert").on("click", function() {
+		if(isCelsius) {
+			$("#temperature").html(fahrenheitTemperature + " &#8457");
+			$("#convert").html("Convert to Celsius");
+			isCelsius = false;
+		} else {
+			$("#temperature").html(celsiusTemperature + " &#8451");
+			$("#convert").html("Convert to Fahrenheit");
+			isCelsius = true;
+		}
+	});
 });
